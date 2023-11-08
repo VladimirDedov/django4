4c894d5221d883faf8bf5241b53c7255a7fd3f2a
VladimirDedov <vldedov@bk.ru>
2023-11-08 16:53:36 +0600

    tkt-002: chenged index.html, urls.py, views.py

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('cats/<slug:cat_slug>/', views.categories, name='cats'),
+    path('addpage/', views.addpage, name='addpage'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:cat_id>/', views.show_post, name='post'),
 ]
