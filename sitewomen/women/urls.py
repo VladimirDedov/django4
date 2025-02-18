3bb70f088d658b439e285babc5aa5d6cc8422f98
Vladimir Dedov <vldedov@bk.ru>
2025-02-18 11:24:19 +0500

    added templates

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('cats/<slug:cat_slug>/', views.categories, name='cats'),
+    path('about/', views.about, name='about'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name = 'post')
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,9 +1,46 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+menu = [{'title': "Главная страница", 'url_name': 'home'},
+        {'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
+
 
 def index(request):
-    return render(request, 'women/index.html' )
+    data = {'title': 'Head page',
+            'menu': menu,
+            'posts': data_db}
+    return render(request, 'women/index.html', context=data)
+
+
+def about(request):
+    data = {'title': 'About page',
+            'menu': menu}
+    return render(request, 'women/about.html', context=data)
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Пост номер {post_id}")
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def categories(request, cat_slug):
